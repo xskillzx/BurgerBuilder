df3d414c630730d60009243637df32709ba58a7e
Moises Munoz <moises_a_munoz@yahoo.com>
2020-01-07 19:16:21 -0800

    add a toolbar to the app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,6 +96,7 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseCancelHandler}
           show={this.state.purchasing}>
           <OrderSummary
+            price={this.state.totalPrice.toFixed(2)}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
             ingredients={this.state.ingredients} />
