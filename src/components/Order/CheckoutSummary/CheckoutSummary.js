117809da63ee3780f4d33f83ff6f9e73886be941
Moises Munoz <moises_a_munoz@yahoo.com>
2020-02-14 12:13:15 -0800

    create contact data form and navigation

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.module.css';
 
-const checkoutSummary = props => {
+const checkoutSummary = (props) => {
   return (
-    <div className={classes.CheckoutSummary}>
-      <h1>We hope it tastes well!</h1>
-      <div style={{
-        width: '100%',
-        margin: 'auto'
-      }}>
-        <Burger ingredients={props.ingredients}/>
-      </div>
-      <Link to='/'>
-        <Button 
-          clicked={props.checkoutCancelled}
-          btnType='Danger'>
-          CANCEL
-        </Button>
-      </Link>
-      <Link to='/checkout'>
-        <Button 
-          clicked={props.checkoutContinued}
-          btnType='Success'>
-          CONTINUE
-        </Button>
-      </Link>
+      <div className={classes.CheckoutSummary}>
+          <h1>We hope it tastes well!</h1>
+          <div style={{width: '100%', margin: 'auto'}}>
+              <Burger ingredients={props.ingredients}/>
+          </div>
+          <Button 
+              btnType="Danger"
+              clicked={props.checkoutCancelled}>CANCEL</Button>
+          <Button 
+              btnType="Success"
+              clicked={props.checkoutContinued}>CONTINUE</Button>
       </div>
   );
 }
