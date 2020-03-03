33b682e93166a40c2a6cd6ab4824628f58da0284
Moises Munoz <moises_a_munoz@yahoo.com>
2020-03-03 13:16:37 -0800

    implement navigation links

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,10 +6,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
     <NavigationItem
-      link='/'
-      active>Burger Builder</NavigationItem>
+      link='/' exact>Burger Builder</NavigationItem>
     <NavigationItem
-      link='/'>My Orders</NavigationItem>
+      link='/orders'>Orders</NavigationItem>
   </ul>
 );
 
