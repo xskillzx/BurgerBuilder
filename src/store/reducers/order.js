ec9f1dad0904ec77963e91b75b58c9a685804f87
Moises Munoz <moises_a_munoz@yahoo.com>
2020-06-16 03:10:26 -0700

    cleanup order reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -4,23 +4,26 @@ import {
 } from '../actions/actionTypes';
 
 const initialState = {
-  error: null,
+  orders: [],
   loading: false
 }
 
 const order = (state = initialState, action) => {
   switch (action.type) {
     case ORDER_SUCCESS:
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+      }
       return {
         ...state,
-        error: false,
-        loading: false
+        loading: false,
+        orders: state.orders.concat(newOrder)
       }
     case ORDER_FAILED:
       return {
         ...state,
-        error: true,
-        loading: false
+        loading: true
       }
     default:
       return state
