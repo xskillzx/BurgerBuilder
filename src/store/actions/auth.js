2453db8456552a5dbeb34a44bf132ca000053239
Moises Munoz <moises_a_munoz@yahoo.com>
2021-02-16 02:49:12 -0800

    get an auth token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[KEY]', authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      })
   };
 };
